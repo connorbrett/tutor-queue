e07a07020ec6bfe3a029be1c09942ee8ac79920b
Hung Le Ba <hungleba@email.arizona.edu>
2021-11-20 15:43:10 -0700

    Add login method

---
+++ public_html/index.js
@@ -0,0 +1,31 @@
+// Popup a board for tutor login
+function loginPopup() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+// Redirect to student's webpage
+function toStudent() {
+    window.location.href = "student.html";
+}
+
+/*  Authenticate and login tutor.
+    Redirect if res.end returns SUCCESS!, alert otherwise 
+*/
+function toTutor() {
+    var accountInfo = {
+        username: $("#netID").val(),
+        password: $("#password").val()
+    };
+    $.ajax({
+        url: '/login/user',
+        data: accountInfo,
+        method: 'POST',
+        success: function (result) {
+            if (result == 'SUCCESS!') {
+                window.location.href = 'tutor.html';
+            } else {
+                alert("Username or Password incorrect!");
+            }
+        }
+    });
+}
