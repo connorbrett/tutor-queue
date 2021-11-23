b7c9be22b3ab106dbf515f0e8ddb2024b75c66b9
Hung Le Ba <hungleba@email.arizona.edu>
2021-11-22 22:34:06 -0700

    Add student.txt and re-indented index.html

---
+++ public_html/student.js
@@ -0,0 +1,15 @@
+function sendRequest() {
+    var requestInfo = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        courses: $("#course").val()
+    };
+    $.ajax({
+        url: '/add/request',
+        data: requestInfo,
+        method: 'POST',
+        success: function (result) {
+            alert(result);
+        }
+    });
+}
