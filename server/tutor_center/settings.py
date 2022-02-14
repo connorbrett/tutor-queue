be9428950d90beaa34421ac222c392ffbc5f0951
Connor Brett <connorbrett.18@gmail.com>
2022-02-14 13:10:49 -0700

    Fixed DB name

--- server/tutor_center/settings.py
+++ server/tutor_center/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = "tutor_center.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
-        "NAME": "dev",
+        "NAME": "tutorQueue",
         "ENFORCE_SCHEMA": False,
         "CLIENT": {
             # "host": "mongodb+srv://admin:test123@dev.vbai1.mongodb.net",
