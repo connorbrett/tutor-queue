39bfde69b22969295bef560b7243f03735a65845
Aramis Sennyey <asennyey@arizona.edu>
2022-04-19 00:10:04 -0700

    remove throttling

--- server/tutor_center/settings.py
+++ server/tutor_center/settings.py
@@ -247,7 +247,6 @@ REST_FRAMEWORK = {
     #     "rest_framework.throttling.AnonRateThrottle",
     #     "rest_framework.throttling.UserRateThrottle",
     # ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
 }
